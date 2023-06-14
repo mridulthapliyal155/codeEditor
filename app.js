200d98d99f5622e260ee2a89c7faf49a300930db
Mridul <thapliyamridul19@gmailcom>
2023-06-14 22:41:57 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const codes = document.querySelectorAll("textarea");
+
+codes.forEach((code) => {
+  code.addEventListener("keyup", run);
+});
+
+function run() {
+  let htmlCode = document.getElementById("html-code").value;
+  let cssCode = document.getElementById("css-code").value;
+  let jsCode = document.getElementById("js-code").value;
+  let output = document.getElementById("output");
+
+  output.contentDocument.body.innerHTML =
+    htmlCode + "<style>" + cssCode + "</style>";
+  output.contentWindow.eval(jsCode);
+}
